bf81210bd30caf75973c304389856907537e6689
Markus-Schwer <markus.schwer@protonmail.com>
2023-09-28 01:26:52 +0200

    add keybinding for toggeling lsp_lines

--- home/nvim/config/lua/markus/lsp_lines.lua
+++ home/nvim/config/lua/markus/lsp_lines.lua
@@ -2,11 +2,18 @@ require('lsp_lines').setup()
 vim.diagnostic.config({ virtual_text = false })
 -- toggle diagnostics to avoid clashes
 local diagnostics_active = true
-vim.keymap.set('n', '<leader>td', function()
+vim.keymap.set('n', '<leader>dt', function()
     diagnostics_active = not diagnostics_active
     if diagnostics_active then
         vim.diagnostic.show()
     else
         vim.diagnostic.hide()
     end
-end, { desc = '[t]oggle [d]iagnostics' })
+end, { desc = '[d]iagnostics [t]oggle ' })
+
+vim.keymap.set(
+  'n',
+  '<leader>lt',
+  function() require('lsp_lines').toggle() end,
+  { desc = 'lsp_[l]ines [t]oggle ' }
+)
