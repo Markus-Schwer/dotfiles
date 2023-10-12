d4df6cf9ce6b7e2a91a7d302067cb58e4ed573db
Markus Schwer <markus.schwer@protonmail.com>
2023-10-12 17:54:25 +0200

    add playerctl keybindings

--- home/sway/sway.nix
+++ home/sway/sway.nix
@@ -186,6 +186,9 @@
           "--locked XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set +5%";
           "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
           "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
+          "XF86AudioPlay" = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+          "XF86AudioNext" = "exec ${pkgs.playerctl}/bin/playerctl next";
+          "XF86AudioPrev" = "exec ${pkgs.playerctl}/bin/playerctl previous";
         };
         modes = {
           "${modeShutdown}" = {
