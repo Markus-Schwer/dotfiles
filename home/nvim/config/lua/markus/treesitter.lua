a44cf6d9a272023419f863458e664a328df9101b
Markus-Schwer <markus.schwer@protonmail.com>
2023-12-20 00:14:27 +0100

    fix treesitter commentstring deprecation warning

--- home/nvim/config/lua/markus/treesitter.lua
+++ home/nvim/config/lua/markus/treesitter.lua
@@ -8,10 +8,4 @@ require('nvim-treesitter.configs').setup({
     incremental_selection = {
         enable = true,
     },
-    -- context commetstring setup
-    -- source: https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    },
 })
