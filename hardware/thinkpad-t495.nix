bf13592eafc7af6494361067597eee24c6d141ee
Markus-Schwer <markus.schwer@protonmail.com>
2024-02-21 22:33:47 +0100

    use disko instead of "manual" disk config for thinknix

--- hardware/thinkpad-t495.nix
+++ hardware/thinkpad-t495.nix
@@ -4,42 +4,18 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ ];
-
   boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" "amdgpu" ];
   boot.extraModulePackages = [ ];
-  boot.initrd.luks.devices = {
-    cryptroot = {
-      device = "/dev/disk/by-label/cryptroot";
-      preLVM = true;
-      allowDiscards = true;
-    };
-  };
-
-  fileSystems."/" =
-    {
-      device = "/dev/vg/root";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-label/boot";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [{ device = "/dev/vg/swap"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
-  hardware.enableRedistributableFirmware = true;
+
+  hardware.enableRedistributableFirmware = true; # required for wifi to work
 
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
