1b2dc45cc0bd682e9b00384cff8dd3290bcbb5a1
Markus-Schwer <markus.schwer@protonmail.com>
2024-03-06 15:17:43 +0100

    add nixpad public keys to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
     thinknix-markus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvLFmp9FQ6rI8FWjxbia/ddGJBGjEdczyGtI8NqKGpn markus@markus-nixos";
     thinknix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBICxcnUgDpM9VDMWV0bBeBmJr2hUJ+E4gSZJwYjWWsw root@markus-nixos";
-    all = [ thinknix thinknix-markus ];
+    nixpad-markus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwGwmrgTrl474NM36MNH/oR+QodDZEwz7jailOnoUQo markus@markus-nixos";
+    nixpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOVioAqB05gkclIgb5uyBuBs9TJE0FxNmvHjFTjUKo9M root@markus-nixos";
+    all = [ thinknix thinknix-markus nixpad nixpad-markus ];
 in
 {
     "sourcegraph.age".publicKeys = all;
