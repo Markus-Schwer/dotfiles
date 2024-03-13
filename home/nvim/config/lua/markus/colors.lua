8d658965bbccf758dcff3dda21d7791e8e6070c6
Markus Schwer <markus.schwer@protonmail.com>
2024-03-13 01:21:40 +0100

    fix transparency issue for lsp lines

--- home/nvim/config/lua/markus/colors.lua
+++ home/nvim/config/lua/markus/colors.lua
@@ -7,7 +7,16 @@ require("transparent").setup({
     'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
     'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
     'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
-    'EndOfBuffer',
+    'EndOfBuffer', 'DiagnosticError', 'DiagnosticWarn', 'DiagnosticInfo',
+    'DiagnosticHint', 'DiagnosticOk', 'DiagnosticVirtualTextError',
+    'DiagnosticVirtualTextError', 'DiagnosticVirtualTextWarn',
+    'DiagnosticVirtualTextInfo', 'DiagnosticVirtualTextHint',
+    'DiagnosticVirtualTextOk', 'DiagnosticUnderlineError', 'DiagnosticUnderlineWarn',
+    'DiagnosticUnderlineInfo', 'DiagnosticUnderlineHint', 'DiagnosticUnderlineOk',
+    'DiagnosticFloatingError', 'DiagnosticFloatingWarn', 'DiagnosticFloatingInfo',
+    'DiagnosticFloatingHint', 'DiagnosticFloatingOk', 'DiagnosticSignError',
+    'DiagnosticSignWarn', 'DiagnosticSignInfo', 'DiagnosticSignHint',
+    'DiagnosticSignOk', 'DiagnosticDeprecated', 'DiagnosticUnnecessary'
   },
 })
 vim.g.transparent_enabled = true
