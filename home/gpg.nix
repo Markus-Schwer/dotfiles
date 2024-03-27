d61916d8cf6c8e59241894e8f5f3fca09b19e2fb
Markus Schwer <markus.schwer@protonmail.com>
2024-03-27 12:18:29 +0100

    nix flake update

--- home/gpg.nix
+++ home/gpg.nix
@@ -4,10 +4,10 @@
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
-    pinentryFlavor = "gnome3";
+    pinentryPackage = pkgs.pinentry-gnome3;
   };
 
   home.packages = with pkgs; [
-    pinentry
+    pinentry-gnome3
   ];
 }
