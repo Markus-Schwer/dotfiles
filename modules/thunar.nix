db0832b0635f7cf1f81054dd46b798e32a33de48
Markus Schwer <markus.schwer@protonmail.com>
2024-04-10 10:49:23 +0200

    add xarchiver for thunar archive integration

--- modules/thunar.nix
+++ modules/thunar.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [ xarchiver ];
   programs.thunar.enable = true;
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin
