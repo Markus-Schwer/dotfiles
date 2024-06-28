2023e7e72679d1036134c59719504e3a01ddf0c0
Markus Schwer <markus.schwer@protonmail.com>
2024-06-28 13:26:32 +0200

    webdav support for thunar

--- modules/thunar.nix
+++ modules/thunar.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ xarchiver ];
+  environment.systemPackages = with pkgs; [ xarchiver davfs2 ];
   programs.thunar.enable = true;
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin
     thunar-volman
   ];
   services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.davfs2.enable = true; # WebDav support (e.g. NextCloud)
   services.tumbler.enable = true; # Thumbnail support for images
 }
