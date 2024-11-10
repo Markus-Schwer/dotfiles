10c8a4a3122d922283592e0ce62d27da408008f4
Markus Schwer <markus.schwer@protonmail.com>
2024-11-10 03:33:39 +0100

    add space white toner printer

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,16 +1,26 @@
 { pkgs, ... }: {
   services.printing = {
     enable = true;
-    drivers = [ pkgs.hplip ];
+    drivers = with pkgs; [ hplip canon-cups-ufr2 ];
   };
-  hardware.printers.ensurePrinters = [{
-    name = "Space-2D-Drucker";
-    model = "HP/hp-color_laserjet_mfp_m480-ps.ppd.gz";
-    deviceUri = "socket://2d.sfz-aalen.space";
-    ppdOptions = {
-      PageSize = "A4";
-    };
-  }];
+  hardware.printers.ensurePrinters = [
+    {
+      name = "Space-2D-Drucker";
+      model = "HP/hp-color_laserjet_mfp_m480-ps.ppd.gz";
+      deviceUri = "socket://10.11.42.10";
+      ppdOptions = {
+        PageSize = "A4";
+      };
+    }
+    {
+      name = "Space-Weisstoner-Drucker";
+      model = "HP/hp-color_laserjet_mfp_m480-ps.ppd.gz";
+      deviceUri = "socket://10.11.42.12";
+      ppdOptions = {
+        PageSize = "A4";
+      };
+    }
+  ];
   services.avahi = {
     enable = true;
     nssmdns4 = true;
