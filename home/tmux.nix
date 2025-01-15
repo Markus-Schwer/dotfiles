4ed78592bb6c9354895afe21a66c26aa44ecab55
Markus Schwer <markus.schwer@protonmail.com>
2025-01-15 16:15:06 +0100

    fix nvim restore in tmux by patching out nix store paths in tmux save files

--- home/tmux.nix
+++ home/tmux.nix
@@ -14,7 +14,13 @@
       }
       {
         plugin = resurrect;
-        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+        extraConfig = ''
+          set -g @resurrect-processes 'nvim btop'
+          set -g @resurrect-strategy-nvim 'session'
+          resurrect_dir=~/.tmux/resurrect
+          set -g @resurrect-dir $resurrect_dir
+          set -g @resurrect-hook-post-save-all "sed -i 's| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/nix/store/.*/bin/||g' $(readlink -f $resurrect_dir/last)"
+        '';
       }
       {
         plugin = continuum;
