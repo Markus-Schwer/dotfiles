ac8bd80b04811867fca3f5828da934caedc31da7
Markus Schwer <markus.schwer@protonmail.com>
2025-03-22 16:47:41 +0100

    add tabset config for Makefiles

--- home/nvim/config/lua/markus/tabset.lua
+++ home/nvim/config/lua/markus/tabset.lua
@@ -5,7 +5,7 @@ require("tabset").setup({
 	},
 	languages = {
 		{
-			filetypes = { "go", "templ", "lua" },
+			filetypes = { "go", "templ", "lua", "make" },
 			config = {
 				tabwidth = 4,
 				expandtab = false,
