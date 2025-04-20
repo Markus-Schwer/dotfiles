99406d59afca5be06192babd410bbcae2c59979f
Markus Schwer <markus.schwer@protonmail.com>
2025-04-21 00:30:52 +0200

    remove commented ouut cppdbg dap config from neovim config

--- pkgs/neovim/config/lua/config/debug.lua.nix
+++ pkgs/neovim/config/lua/config/debug.lua.nix
@@ -14,11 +14,6 @@
     },
   }
 
-  #dap.adapters.cppdbg = {
-  #  id = 'cppdbg',
-  #  type = 'executable',
-  #  command = ''${pkgs.vscode-extensions.ms-vscode.cpptools}/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7',
-  #}
   dap.configurations.cpp = {
     {
       name = "Launch file",
