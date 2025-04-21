3435ea60a815e48b386b4f6c73a5d64bf4d9363e
Markus Schwer <markus.schwer@protonmail.com>
2025-04-21 10:09:47 +0200

    chore(hosts): remove dankpad

--- hosts.nix
+++ hosts.nix
@@ -45,43 +45,4 @@
       }
     ];
   }
-  {
-    name = "dankpad";
-    system = "x86_64-linux";
-    nixosModules = [
-      disko.nixosModules.disko
-      ./hardware/thinkpad-l590.nix
-      (import ./disko-config.nix { disk = "/dev/nvme0n1"; })
-      {
-        markus.network = {
-          hostname = "nixpad";
-          hostid = "ea12bde6";
-        };
-        markus.k3s.enable = true;
-        services.k3s.extraFlags = toString [
-          "--disable=traefik"
-        ];
-        networking.extraHosts = ''
-          192.168.178.147 keycloak.fcp.local
-          192.168.178.147 fnt-application.fcp.local
-          192.168.178.147 fcp-master.fcp.local
-          192.168.178.147 grafana.fcp.local
-          192.168.178.147 command.fcp.local
-          192.168.178.147 minio.fcp.local
-          192.168.178.147 minio-console.fcp.local
-          192.168.178.147 minio-backup.fcp.local
-          192.168.178.147 camunda.fcp.local
-          192.168.178.147 serviceplanet.fcp.local
-          192.168.178.147 analytics.fcp.local
-          192.168.178.147 adminer.fcp.local
-          192.168.178.147 keycloak.smoke.local
-          192.168.178.147 fnt-application.smoke.local
-          192.168.178.147 fnt-application.fcp-clone.local
-          192.168.178.147 minio.fcp-clone.local
-          192.168.178.147 analytics.fcp-clone.local
-          192.168.178.147 fnt-monitoring.fcp.local
-        '';
-      }
-    ];
-  }
 ]
