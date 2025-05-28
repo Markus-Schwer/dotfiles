e4910f774f0e0ab68bec1e53d6b6e2ce416d5b7e
Markus Schwer <markus.schwer@protonmail.com>
2025-05-28 08:58:42 +0200

    rework trouble keybindings and fix diagnostic modes

--- pkgs/neovim/config/lua/config/trouble.lua
+++ pkgs/neovim/config/lua/config/trouble.lua
@@ -1,20 +1,37 @@
-require("trouble").setup()
+require("trouble").setup({
+	modes = {
+		workspace_diagnostics = {
+			mode = "diagnostics",
+			filter = {
+				{
+					function (item)
+						return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+					end
+				}
+			},
+		},
+		document_diagnostics = {
+			mode = "diagnostics",
+			filter = { buf = 0 },
+		},
+	},
+})
 
-vim.keymap.set("n", "<leader>ts", function()
-	require("trouble").open()
-end, { desc = "[t]roubles [s]how" })
+vim.keymap.set("n", "<leader>tt", function()
+	require("trouble").toggle("diagnostics")
+end, { desc = "[t]roubles [t]oggle" })
 vim.keymap.set("n", "<leader>tw", function()
-	require("trouble").open("workspace_diagnostics")
-end, { desc = "[t]rouble [w]orkspace diagnostics" })
+	require("trouble").toggle("workspace_diagnostics")
+end, { desc = "[t]rouble toggle [w]orkspace diagnostics" })
 vim.keymap.set("n", "<leader>td", function()
-	require("trouble").open("document_diagnostics")
-end, { desc = "[t]rouble [d]ocument diagnostics" })
+	require("trouble").toggle("document_diagnostics")
+end, { desc = "[t]rouble toggle [d]ocument diagnostics" })
 vim.keymap.set("n", "<leader>tq", function()
-	require("trouble").open("quickfix")
-end, { desc = "[t]rouble [q]uickfix" })
+	require("trouble").toggle("quickfix")
+end, { desc = "[t]rouble toggle [q]uickfix" })
 vim.keymap.set("n", "<leader>tl", function()
-	require("trouble").open("loclist")
-end, { desc = "[t]rouble [l]oclist" })
+	require("trouble").toggle("loclist")
+end, { desc = "[t]rouble toggle [l]oclist" })
 vim.keymap.set("n", "gR", function()
-	require("trouble").open("lsp_references")
+	require("trouble").toggle("lsp_references")
 end, { desc = "[g] go to trouble lsp [R]eferences" })
