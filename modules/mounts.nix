7a028b3182257ae4fbf47da1d17b23da29d9029b
Markus Schwer <markus.schwer@protonmail.com>
2025-06-16 22:54:17 +0200

    remove broken mounts

--- modules/mounts.nix
+++ modules/mounts.nix
@@ -14,53 +14,53 @@
     };
   };
 
-  fileSystems."/mnt/talos" = {
-    fsType = "nfs";
-    device = "10.20.42.50:/talos";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=30" # disconnects after 30 seconds
-    ];
-  };
+  #fileSystems."/mnt/talos" = {
+  #  fsType = "nfs";
+  #  device = "10.20.42.50:/talos";
+  #  options = [
+  #    "x-systemd.automount"
+  #    "noauto"
+  #    "x-systemd.idle-timeout=30" # disconnects after 30 seconds
+  #  ];
+  #};
 
-  fileSystems."/mnt/k3s" = {
-    fsType = "nfs";
-    device = "10.20.42.50:/kubernetes";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=30" # disconnects after 30 seconds
-    ];
-  };
+  #fileSystems."/mnt/k3s" = {
+  #  fsType = "nfs";
+  #  device = "10.20.42.50:/kubernetes";
+  #  options = [
+  #    "x-systemd.automount"
+  #    "noauto"
+  #    "x-systemd.idle-timeout=30" # disconnects after 30 seconds
+  #  ];
+  #};
 
-  fileSystems."/mnt/lasercutter" = {
-    fsType = "nfs";
-    device = "10.20.42.50:/Lasercutter";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=30" # disconnects after 30 seconds
-    ];
-  };
+  #fileSystems."/mnt/lasercutter" = {
+  #  fsType = "nfs";
+  #  device = "10.20.42.50:/Lasercutter";
+  #  options = [
+  #    "x-systemd.automount"
+  #    "noauto"
+  #    "x-systemd.idle-timeout=30" # disconnects after 30 seconds
+  #  ];
+  #};
 
-  fileSystems."/mnt/music" = {
-    fsType = "nfs";
-    device = "10.20.42.50:/Music";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=30" # disconnects after 30 seconds
-    ];
-  };
+  #fileSystems."/mnt/music" = {
+  #  fsType = "nfs";
+  #  device = "10.20.42.50:/Music";
+  #  options = [
+  #    "x-systemd.automount"
+  #    "noauto"
+  #    "x-systemd.idle-timeout=30" # disconnects after 30 seconds
+  #  ];
+  #};
 
-  fileSystems."/mnt/scans" = {
-    fsType = "nfs";
-    device = "10.20.42.50:/Scans";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=30" # disconnects after 30 seconds
-    ];
-  };
+  #fileSystems."/mnt/scans" = {
+  #  fsType = "nfs";
+  #  device = "10.20.42.50:/Scans";
+  #  options = [
+  #    "x-systemd.automount"
+  #    "noauto"
+  #    "x-systemd.idle-timeout=30" # disconnects after 30 seconds
+  #  ];
+  #};
 }
