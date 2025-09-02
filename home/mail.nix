7fb0dd096ed2f0296fdc1ff71afa89d6fa66230a
Markus Schwer <markus.schwer@protonmail.com>
2025-09-02 21:37:22 +0200

    add protonmail account to thunderbird

--- home/mail.nix
+++ home/mail.nix
@@ -9,7 +9,7 @@
       realName = "Markus Schwer";
       address = "markus.schwer@aalen.space";
       userName = "markus.schwer@aalen.space";
-      primary = true;
+      primary = false;
       thunderbird = {
         enable = true;
       };
@@ -23,6 +23,32 @@
         host = "mail.aalen.space";
         port = 465;
         tls.enable = true;
+      };
+    };
+    protonmail = {
+      realName = "Markus Schwer";
+      address = "markus.schwer@protonmail.com";
+      userName = "markus.schwer@protonmail.com";
+      primary = true;
+      thunderbird = {
+        enable = true;
+      };
+
+      imap = {
+        host = "127.0.0.1";
+        port = 1143;
+        tls = {
+          enable = true;
+          useStartTls = true;
+        };
+      };
+      smtp = {
+        host = "127.0.0.1";
+        port = 1025;
+        tls = {
+          enable = true;
+          useStartTls = true;
+        };
       };
     };
   };
